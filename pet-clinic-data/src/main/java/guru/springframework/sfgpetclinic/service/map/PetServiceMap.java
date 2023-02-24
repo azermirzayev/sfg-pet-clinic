0eb0b72602a32d83beb548ad5cc93c3bfb9b1c47
azer-mirzayev <azer.mirzayev@digirella.az>
2023-02-24 14:05:45 +0400

    Adding spring config

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/PetServiceMap.java
@@ -3,7 +3,9 @@ package guru.springframework.sfgpetclinic.service.map;
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.service.PetService;
 import java.util.Set;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetServiceMap.java
@@ -3,7 +3,9 @@ package guru.springframework.sfgpetclinic.service.map;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.service.VetService;
 import java.util.Set;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
